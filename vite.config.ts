7717d9fcbabbdf5a43faa7ecc337f59a12795497
Matt Powell <matt.powell@usabilityhub.com>
2024-03-24 23:20:16 +1300

    disable million lint

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import * as path from 'node:path';
-import MillionLint from '@million/lint';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
 
 export default defineConfig({
-  plugins: [MillionLint.vite(), react(), TanStackRouterVite()],
+  plugins: [react(), TanStackRouterVite()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
