33ea3122cc1aec23dcf88a2744964813ed3ac3dd
Matt Powell <matt.powell@usabilityhub.com>
2024-07-30 14:41:29 +1200

    only use emulators in dev

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,21 +1,21 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
-import { connectDatabaseEmulator, getDatabase } from "firebase/database";
-import { connectStorageEmulator, getStorage } from "firebase/storage";
+import { initializeApp } from 'firebase/app';
+import { connectFirestoreEmulator, getFirestore } from 'firebase/firestore';
+import { connectDatabaseEmulator, getDatabase } from 'firebase/database';
+import { connectStorageEmulator, getStorage } from 'firebase/storage';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyAc7xzH2DsoSRRMrKuz47CHedwmFY7cM9U",
-  authDomain: "thesaboteur.firebaseapp.com",
-  projectId: "thesaboteur",
-  storageBucket: "thesaboteur.appspot.com",
-  messagingSenderId: "35833465655",
-  appId: "1:35833465655:web:f7e6015f41764bbfad28b2",
-  measurementId: "G-5JDPJK83YZ",
+  apiKey: 'AIzaSyAc7xzH2DsoSRRMrKuz47CHedwmFY7cM9U',
+  authDomain: 'thesaboteur.firebaseapp.com',
+  projectId: 'thesaboteur',
+  storageBucket: 'thesaboteur.appspot.com',
+  messagingSenderId: '35833465655',
+  appId: '1:35833465655:web:f7e6015f41764bbfad28b2',
+  measurementId: 'G-5JDPJK83YZ',
 };
 
 // Initialize Firebase
@@ -24,6 +24,8 @@ export const db = getFirestore(app);
 export const realtime = getDatabase(app);
 export const storage = getStorage(app);
 
-connectFirestoreEmulator(db, "localhost", 8081);
-connectDatabaseEmulator(realtime, "localhost", 9000);
-connectStorageEmulator(storage, "localhost", 9199);
+if (import.meta.env.DEV) {
+  connectFirestoreEmulator(db, 'localhost', 8081);
+  connectDatabaseEmulator(realtime, 'localhost', 9000);
+  connectStorageEmulator(storage, 'localhost', 9199);
+}
