0a8d3f328887217d48c93f129d6a1e8cdcb17010
Matt Powell <matt.powell@usabilityhub.com>
2024-08-01 08:55:43 +1200

    add new default questions

--- src/hooks/useEvents.ts
+++ src/hooks/useEvents.ts
@@ -1,33 +1,19 @@
-import { db } from "@/firebase";
-import { Mission, PartialWithId } from "@/types";
-import { Event, EventFirebaseSchema, EventSchema } from "@/types/Event";
-import { DEFAULT_QUESTIONS, Question } from "@/types/Question";
-import {
-  addDoc,
-  collection,
-  deleteDoc,
-  doc,
-  onSnapshot,
-  updateDoc,
-} from "firebase/firestore";
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { db } from '@/firebase';
+import { Mission, PartialWithId } from '@/types';
+import { Event, EventFirebaseSchema, EventSchema } from '@/types/Event';
+import { DEFAULT_QUESTIONS, Question } from '@/types/Question';
+import { addDoc, collection, deleteDoc, doc, onSnapshot, updateDoc } from 'firebase/firestore';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 
 export const useEvents = (mission: Mission) => {
-  const ref = useMemo(
-    () => collection(db, "missions", mission.id, "events"),
-    [mission.id]
-  );
+  const ref = useMemo(() => collection(db, 'missions', mission.id, 'events'), [mission.id]);
 
   const [events, setEvents] = useState<Event[]>([]);
 
   useEffect(
     () =>
       onSnapshot(ref, (snapshot) =>
-        setEvents(
-          snapshot.docs.map((doc) =>
-            EventSchema.parse({ id: doc.id, ...doc.data() })
-          )
-        )
+        setEvents(snapshot.docs.map((doc) => EventSchema.parse({ id: doc.id, ...doc.data() })))
       ),
     [ref]
   );
@@ -36,38 +22,57 @@ export const useEvents = (mission: Mission) => {
     const doc = await addDoc(
       ref,
       EventFirebaseSchema.parse({
-        type: "quiz",
+        type: 'quiz',
         timestamp: new Date(),
         startsAt: null,
         endsAt: null,
       })
     );
 
-    const questionsRef = collection(ref, doc.id, "questions");
+    const questionsRef = collection(ref, doc.id, 'questions');
 
-    const addQuestion = async (type: Question["type"], position: number) => {
-      const multiple = type === "scenes";
+    const addQuestion = async (
+      type: Question['type'],
+      position: number,
+      label?: string,
+      options?: string[]
+    ) => {
+      const multiple = type === 'scenes';
       const data = {
         type,
         multiple,
-        label: DEFAULT_QUESTIONS[type],
+        label: label ?? DEFAULT_QUESTIONS[type],
         position,
       };
-      await addDoc(questionsRef, data);
+      const question = await addDoc(questionsRef, data);
+      if (options) {
+        const answersRef = collection(questionsRef, question.id, 'answers');
+        options.forEach(async (label, position) => {
+          await addDoc(answersRef, { label, position, correct: false });
+        });
+      }
     };
 
-    await addQuestion("scenes", 0);
-    await addQuestion("suspicional", 1);
-    await addQuestion("accusation", 2);
+    await addQuestion('custom', 0, 'When did the Saboteur introduce themselves?', [
+      'First three',
+      'Last two',
+    ]);
+    await addQuestion('custom', 1, 'Where is the Saboteur sitting?', [
+      'Desk side',
+      'Not desk side',
+    ]);
+    await addQuestion('scenes', 2);
+    await addQuestion('suspicional', 3);
+    await addQuestion('accusation', 4);
   }, [ref]);
 
   const createScene = useCallback(async () => {
     await addDoc(
       ref,
       EventFirebaseSchema.parse({
-        type: "scene",
+        type: 'scene',
         timestamp: new Date(),
-        name: "",
+        name: '',
         sabotaged: false,
       })
     );
@@ -77,7 +82,7 @@ export const useEvents = (mission: Mission) => {
     await addDoc(
       ref,
       EventFirebaseSchema.parse({
-        type: "suspicional",
+        type: 'suspicional',
         timestamp: new Date(),
         accused: [],
       })
@@ -86,7 +91,7 @@ export const useEvents = (mission: Mission) => {
 
   const updateEvent = useCallback(
     async (event: PartialWithId<Event>) => {
-      const ref = doc(db, "missions", mission.id, "events", event.id);
+      const ref = doc(db, 'missions', mission.id, 'events', event.id);
       const updates = EventFirebaseSchema.parse(event);
       await updateDoc(ref, updates);
     },
